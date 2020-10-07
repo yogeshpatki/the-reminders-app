35bd1c4b23323dfce205b096eda2c9cf7f46a04c
Yogesh Patki <yogesh.patki@gmail.com>
2020-10-07 19:53:33 +0530

    more config changes

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+export const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_PROJECT_ID,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  };
+  
+  export const VAPID_KEY = process.env.REACT_APP_VAPID_KEY;
+  
+  
